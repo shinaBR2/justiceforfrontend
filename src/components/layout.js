71ab7f2eae610b5b3b1275c1416e5fe1b62d9b90
ShinaBR2 <ShinaBR2@gmail.com>
2021-01-17 11:12:54 +0700

    First content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from './header'
-import './layout.css'
+import Header from "./header";
+import "./layout.css";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -16,15 +16,15 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
+    render={(data) => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
           {children}
@@ -32,10 +32,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
